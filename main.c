86c407e3e90616725aa00f99ee87293927be9dda
Grupo 3 <grupo3@gmail.com>
2025-04-09 18:51:59 +0200

    Se introduce lógica para preguntar por el num de radares

--- main.c
+++ main.c
@@ -6,7 +6,10 @@ int suma(int a, int b) {
 }
 
 int main() {
+    int num_radares;
     int resultado = suma(3, 4);
     printf("Resultado: %d\n", resultado);
+    printf("Cuantos radares quieres comprar?\n");
+    scanf("%d", num_radares);
     return 0;
 }
